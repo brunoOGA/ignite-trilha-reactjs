f1526aea895b5a02d2159f466bee8d29de77317c
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-05-17 07:51:55 -0300

    Finalizando inserção

--- 02-dtmoney/src/TransactionsContext.tsx
+++ 02-dtmoney/src/TransactionsContext.tsx
@@ -27,7 +27,7 @@ interface TransactionProviderProps {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transactionInput: TransactionInput) => Promise<void>;
 }
 
 export const TransactionContext = createContext<TransactionsContextData>(
@@ -42,8 +42,15 @@ export function TransactionsProvider({children}: TransactionProviderProps) {
       .then(response => setTransactions(response.data.transactions))
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('/transactions', transaction)
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+
+    const { transaction } = response.data;
+
+    setTransactions([...transactions, transaction]);
   }
 
   return (
