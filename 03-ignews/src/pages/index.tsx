f0dd7c1c4c3e2297e03fc60c9bbd24e78b68dd01
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-05-25 12:22:30 -0300

    Static Site Generation (SSG)

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -11,6 +11,7 @@ interface HomeProps {
     amount: number;
   }
 }
+
 export default function Home({product}: HomeProps) {
   return (
     <>
@@ -34,7 +35,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IuiWsGRMWx1CmmW0p96616z');
 
   const product = {
@@ -44,10 +45,11 @@ export const getServerSideProps: GetServerSideProps = async () => {
       currency: 'USD'
     }).format(price.unit_amount / 100),
   }
-
+ 
   return { 
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24h
   }
 }
