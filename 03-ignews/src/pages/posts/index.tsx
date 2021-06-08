d11dd68484beccf86f4c67a47bcd6777b703534c
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-06-08 14:28:16 -0300

    Página: Post

--- 03-ignews/src/pages/posts/index.tsx
+++ 03-ignews/src/pages/posts/index.tsx
@@ -5,6 +5,7 @@ import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
+import Link from 'next/link';
 
 type Post = {
   slug: string;
@@ -28,11 +29,13 @@ export default function Post({ posts }: PostsProps) {
         <div className={styles.posts}>
           {
             posts.map(post => (
-              <a key={post.slug} href="#">
-                <time>{post.updatedAt}</time>
-                <strong>{post.title}</strong>
-                <p>{post.excerpt}</p>
-              </a>
+              <Link href={`/posts/${post.slug}`}>
+                <a key={post.slug} >
+                  <time>{post.updatedAt}</time>
+                  <strong>{post.title}</strong>
+                  <p>{post.excerpt}</p>
+                </a>
+              </Link>
             ))
           }
         </div>
@@ -41,7 +44,7 @@ export default function Post({ posts }: PostsProps) {
   );
 }
 
-export const getStaticProps: GetStaticProps = async () => { 
+export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
   const response = await prismic.query([
